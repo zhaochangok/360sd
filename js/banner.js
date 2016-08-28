3579e2b0b42fe7e6e8edd8d44ed91347b21f061f
赵昌 <821442330@qq.com>
2016-08-28 14:43:06 +0800

    chush

---
+++ js/banner.js
@@ -0,0 +1,17 @@
+    var swiper = new Swiper('.banner .swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        // spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false
+    });
+    var swiper = new Swiper('.buybanner .swiper-container', {
+        // pagination: '.swiper-pagination',
+        slidesPerView: 3,
+        paginationClickable: true,
+        slidesPerGroup: 3
+        // spaceBetween: 30
+    });
